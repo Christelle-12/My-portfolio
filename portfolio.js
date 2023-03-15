568cda4e3a8917a604132f15d8f5c2bf97848e1d
Christelle Nirere <mariechristellenirere@gmail.com>
2023-03-15 08:02:56 +0200

    Removed commented codes

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,3 @@
-// const togglerButton = document.querySelector('#menuIcon');
-// const displayElement = document.querySelector('.desktop-list');
-// const exitIcon = document.querySelector('#exit-icon');
-// const body = document.querySelector('body')
-
-// togglerButton.addEventListener('click', () => {
-//   displayElement.style.display = 'block';
-//   togglerButton.style.display = 'none'
-//   console.log('#exitIcon')
-//   body.classList.toggle('open');
-
-// });
 const toggle = document.querySelector('.toggle');
 const body = document.querySelector('body');
 const menuOptions = document.querySelectorAll('.desktop-list a');
