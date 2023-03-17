6b3c953f3d7834bf345821f6ae3d05f09ab51268
Christelle Nirere <mariechristellenirere@gmail.com>
2023-03-17 14:10:08 +0200

    create from validation

--- portfolio.js
+++ portfolio.js
@@ -16,3 +16,27 @@ menuBtn.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   menuSection.classList.remove('visible');
 });
+ 
+// form validation
+// get form elements
+const form = document.querySelector('.form');
+const emailInput = document.getElementById('email');
+const submitBtn = form.querySelector('.see-project');
+// add event listener to the form submission
+form.addEventListener('submit', (event) => {
+  // prevent default form submission behavior
+  event.preventDefault();
+
+// check if email is in lowercase
+if (emailInput.value === emailInput.value.toLowerCase()) {
+// email is in lowercase, submit the form
+  form.submit();
+} else {
+  // email is not in lowercase, show error message
+  const errorMsg = document.querySelector('.error-message');
+  errorMsg.textContent = 'Email should be in lowercase.';
+  errorMsg.style.color = '#FF4136';
+  submitBtn.insertAdjacentElement('afterend', errorMsg);
+   }
+});
+
