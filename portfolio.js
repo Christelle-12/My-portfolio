ba37468d34c3f11ef4a6f4deefc072085be95034
Chritelle-12 <mariechristellenirere@gmail.com>
2023-10-14 17:13:34 +0200

    Add some transitions on the cards

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById('menu-close-btn');
 const menuSection = document.querySelector('section.mobile-menu');
 const closeButton = document.getElementById('close-btn');
 const menuItems = document.getElementsByClassName('menu-item');
+const cards = document.querySelector('projects')
 
 for (let i = 0; i < menuItems.length; i += 1) {
   menuItems[i].addEventListener('click', () => {
